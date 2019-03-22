4522ab7f2219d95f25d0e7c1558883bacc97e92e
higekick <yutoikegami@gmail.com>
2019-03-23 01:36:53 +0900

    create sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    # if you use byebug, uncomment below
+    # debugger
   end
+
+  def new
+    @user = User.new
+    # debugger
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
